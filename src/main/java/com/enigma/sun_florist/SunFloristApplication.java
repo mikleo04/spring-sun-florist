ef07885e77dd2c04d0923635d3b41db2d879a26f
Hafid Siraj Aurakhmah Witra <hafidsiraj.aw@gmail.com>
2024-03-16 12:52:56 +0700

    initial commit

---
+++ src/main/java/com/enigma/sun_florist/SunFloristApplication.java
@@ -0,0 +1,13 @@
+package com.enigma.sun_florist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunFloristApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SunFloristApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/enigma/sun_florist/SunFloristApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enigma.sun_florist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunFloristApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
