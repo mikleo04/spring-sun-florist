b22cde20db74d9ea6243f76a689793ef348c9061
Hafid Siraj Aurakhmah Witra <hafidsiraj.aw@gmail.com>
2024-03-17 10:16:49 +0700

    [IMPROVE] add error handling ResponseStatusException

--- src/main/java/com/enigma/sun_florist/controller/ErrorController.java
+++ src/main/java/com/enigma/sun_florist/controller/ErrorController.java
@@ -7,9 +7,20 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestControllerAdvice
 public class ErrorController {
+
+    @ExceptionHandler({ResponseStatusException.class})
+    public ResponseEntity<CommonResponse<?>> responseStatusExceptionHandler(ResponseStatusException exception) {
+        CommonResponse<?> response = CommonResponse.builder()
+                .statusCode(exception.getStatusCode().value())
+                .message(exception.getReason())
+                .build();
+
+        return ResponseEntity.status(exception.getStatusCode()).body(response);
+    }
 
     @ExceptionHandler({DataIntegrityViolationException.class})
     public ResponseEntity<CommonResponse<?>> dataIntegrityViolationExceptionHandler(DataIntegrityViolationException e) {
