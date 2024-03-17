d6213d49ce8d9dc60cc64d3a669b14f2bd1a08d3
Hafid Siraj Aurakhmah Witra <hafidsiraj.aw@gmail.com>
2024-03-17 12:40:45 +0700

    [FIX] fix regex birthDate

--- src/main/java/com/enigma/sun_florist/dto/request/CustomerRequest.java
+++ src/main/java/com/enigma/sun_florist/dto/request/CustomerRequest.java
@@ -1,5 +1,6 @@
 package com.enigma.sun_florist.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.Column;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
@@ -22,7 +23,7 @@ public class CustomerRequest {
 
     private String address;
 
-//    @Pattern(message = "birth date is invalid", regexp = "^\\d{4}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])$")
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private Date birthDate;
 
     @Column(name = "status")
