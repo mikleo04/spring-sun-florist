65f232bd06d3e1edc83158d094f97031aee48d3c
Hafid Siraj Aurakhmah Witra <hafidsiraj.aw@gmail.com>
2024-03-17 08:50:12 +0700

    [IMPROVE] add error handling DataIntegrityViolationException

---
+++ src/main/java/com/enigma/sun_florist/controller/ErrorController.java
@@ -0,0 +1,41 @@
+package com.enigma.sun_florist.controller;
+
+import com.enigma.sun_florist.dto.response.CommonResponse;
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ErrorController {
+
+    @ExceptionHandler({DataIntegrityViolationException.class})
+    public ResponseEntity<CommonResponse<?>> dataIntegrityViolationExceptionHandler(DataIntegrityViolationException e) {
+        CommonResponse.CommonResponseBuilder<Object> builder = CommonResponse.builder();
+
+        HttpStatus httpStatus;
+
+        if (e.getMessage().contains("foreign key constraint")) {
+            builder.statusCode(HttpStatus.BAD_REQUEST.value());
+            builder.message("Cannot delete data because there is a reference from another table");
+            httpStatus = HttpStatus.BAD_REQUEST;
+        } else if (e.getMessage().contains("unique constraint") || e.getMessage().contains("Duplicate entry")) {
+            builder.statusCode(HttpStatus.CONFLICT.value());
+            builder.message("Data already exist");
+            httpStatus = HttpStatus.CONFLICT;
+        } else if (e.getMessage().contains("violates check constraint")) {
+            builder.statusCode(HttpStatus.BAD_REQUEST.value());
+            builder.message("Insufficient stock");
+            httpStatus = HttpStatus.BAD_REQUEST;
+        } else {
+            builder.statusCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
+            builder.message("Internal Server Error");
+            httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
+        }
+
+        return ResponseEntity.status(httpStatus).body(builder.build());
+    }
+
+}
